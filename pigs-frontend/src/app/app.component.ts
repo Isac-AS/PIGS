0fb5daf1a65e6db10b57a9e3cf9e4d98efcf2014
dcastellanoo <diegobmg36@gmail.com>
2023-05-13 11:48:29 +0100

    Add nav icons

--- pigs-frontend/src/app/app.component.ts
+++ pigs-frontend/src/app/app.component.ts
@@ -5,19 +5,21 @@ import { GlobalService } from './services/global.service';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 export interface CRUD_ARRAY_ENTRY {
-  title: string,
-  routes: CRUD_ARRAY_ENTRY_ROUTES[],
+  title: string;
+  routes: CRUD_ARRAY_ENTRY_ROUTES[];
+  icon: string;
 }
 
 export interface CRUD_ARRAY_ENTRY_ROUTES {
-  route: string,
-  name: string,
+  route: string;
+  name: string;
+  icon: string;
 }
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'TFG_frontend';
@@ -26,66 +28,83 @@ export class AppComponent {
 
   CRUD_ARRAY: CRUD_ARRAY_ENTRY[] = [
     {
-      title: "Menu", 
+      title: 'Menu',
       routes: [
-        {route: "/add_menu", name: "Add Menu"},
-        {route: "/manage_menus", name: "Manage Menus"},
-      ]
+        { route: '/add_menu', name: 'Add Menu', icon: 'add' },
+        { route: '/manage_menus', name: 'Manage Menus', icon: 'food_bank' },
+      ],
+      icon: 'fastfood',
     },
     {
-      title: "Dish", 
+      title: 'Dish',
       routes: [
-        {route: "/add_dish", name: "Add Dish"},
-        {route: "/manage_dishes", name: "Manage Dishes"},
-      ]
+        { route: '/add_dish', name: 'Add Dish', icon: 'add' },
+        {
+          route: '/manage_dishes',
+          name: 'Manage Dishes',
+          icon: 'lunch_dining',
+        },
+      ],
+      icon: 'restaurant_menu',
     },
     {
-      title: "Ingredients", 
+      title: 'Ingredients',
       routes: [
-        {route: "/add_ingredient", name: "Add Ingredient"},
-        {route: "/manage_ingredients", name: "Manage Ingredients"},
-      ]
+        { route: '/add_ingredient', name: 'Add Ingredient', icon: 'add' },
+        {
+          route: '/manage_ingredients',
+          name: 'Manage Ingredients',
+          icon: 'kitchen',
+        },
+      ],
+      icon: 'egg',
     },
     {
-      title: "Aux Items", 
+      title: 'Aux Items',
       routes: [
-        {route: "/add_aux", name: "Add Items"},
-        {route: "/manage_aux", name: "Manage Items"},
-      ]
+        { route: '/add_aux', name: 'Add Items', icon: 'add' },
+        { route: '/manage_aux', name: 'Manage Items', icon: 'mop' },
+      ],
+      icon: 'clean_hands',
     },
     {
-      title: "Providers", 
+      title: 'Providers',
       routes: [
-        {route: "/add_providers", name: "Add Provider"},
-        {route: "/manage_providers", name: "Manage Provider"},
-      ]
+        { route: '/add_providers', name: 'Add Provider', icon: 'add' },
+        {
+          route: '/manage_providers',
+          name: 'Manage Provider',
+          icon: 'shopping_cart',
+        },
+      ],
+      icon: 'add_shopping_cart',
     },
-  ]
+  ];
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
     .pipe(
-      map(result => result.matches),
+      map((result) => result.matches),
       shareReplay()
     );
 
   constructor(
     private breakpointObserver: BreakpointObserver,
-    public globalService: GlobalService,
+    public globalService: GlobalService
   ) {
     // Subscription to pageName. All pages must change the page name in their constructor
     this.globalService.pageName.subscribe({
-      next: newValue => {
+      next: (newValue) => {
         this.currentPageName = newValue.currentPageName;
-      }
-    })
-    
-    // Subscription to global service as the service that will have the visibility information 
+      },
+    });
+
+    // Subscription to global service as the service that will have the visibility information
     // controlling what is seen by the user depending on the role and logged in status.
     this.globalService.loggedInfo.subscribe({
-      next: newValue => {
+      next: (newValue) => {
         this.currentLoggedProfile = newValue.user.profile;
-      }
-    })
-    
+      },
+    });
   }
 }
